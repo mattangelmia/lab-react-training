89ef34e24f8b0cdd7586baf9ad36fb0d4bd7f5b2
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-09-15 15:59:53 -0400

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IdCard
+        img="https://randomuser.me/api/portraits/men/32.jpg"
+        firstName="John"
+        lastName="Doe"
+        gender="male"
+        height="178"
+        birth="1992-07-14"
+      />
+      <IdCard
+        img="https://randomuser.me/api/portraits/women/41.jpg"
+        firstName="Obrien"
+        lastName="Delores"
+        gender="female"
+        height="172"
+        birth="1988-05-11"
+      />
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Random small={1} big={6}></Random>
+      <Random small={1} big={100}></Random>
     </div>
   );
 }
-
+function IdCard(props) {
+  return (
+    <div className="IdCard box">
+      <p>
+        <img src={props.img} alt="Profile Pic" />
+      </p>
+      <p>First Name: {props.firstName}</p>
+      <p>Last Name: {props.lastName}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Height: {props.height}</p>
+      <p>Date of Birth: {props.birth}</p>
+    </div>
+  );
+}
+function Greetings(props) {
+  let greet = {
+    en: 'Hello',
+    fr: 'Bonjour',
+    de: 'Hallo',
+  };
+  return (
+    <div className="IdCard box">
+      {greet[props.lang]} {props.children}
+    </div>
+  );
+}
+function Random(props) {
+  return (
+    <div className="IdCard box">
+      {props.small} {props.big}
+      <h2>
+        Random value between {props.small} and {props.big} =>{' '}
+        {Math.ceil(Math.random() * props.big)}
+      </h2>
+    </div>
+  );
+}
 export default App;
